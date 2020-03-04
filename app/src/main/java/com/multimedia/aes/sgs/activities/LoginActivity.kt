3757825142fbf5dccc8494b7c7f0b2aaefc3ae48
vicen <1bhavicente@gmail.com>
2020-03-04 19:14:24 +0100

    packages crete

--- app/src/main/java/com/multimedia/aes/sgs/activities/LoginActivity.kt
+++ app/src/main/java/com/multimedia/aes/sgs/activities/LoginActivity.kt
@@ -1,12 +1,17 @@
-package com.multimedia.aes.sgs
+package com.multimedia.aes.sgs.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.multimedia.aes.sgs.R
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    private fun initLogin(){
+
     }
 }
